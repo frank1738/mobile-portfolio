d6a74c8a94923c22b63a3a137043e0cb2905898f
frank1738 <98453979+frank1738@users.noreply.github.com>
2022-04-04 16:13:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
-const mobileLinks = document.querySelectorAll(".mob-link");
-const menuTab = document.querySelector(".mob-menu");
-const hamburgerBtn = document.querySelector(".fa-bars");
-const removeBtn = document.querySelector(".fa-xmark");
-const desktopLinks = document.querySelectorAll(".menu-link");
-const homeLink = document.querySelector(".desktop");
-hamburgerBtn.addEventListener("click", showMenuTab);
-removeBtn.addEventListener("click", removeMenuTab);
-homeLink.classList.add("selected");
-homeLink.addEventListener("click", select);
+const mobileLinks = document.querySelectorAll('.mob-link');
+const menuTab = document.querySelector('.mob-menu');
+const hamburgerBtn = document.querySelector('.fa-bars');
+const removeBtn = document.querySelector('.fa-xmark');
+const desktopLinks = document.querySelectorAll('.menu-link');
+const homeLink = document.querySelector('.desktop');
+
+homeLink.classList.add('selected');
+
 function showMenuTab() {
-  menuTab.style.width = "100%";
+  menuTab.style.width = '100%';
 }
 
 function removeMenuTab() {
-  menuTab.style.width = "0%";
+  menuTab.style.width = '0%';
 }
 
 mobileLinks.forEach((link) => {
-  link.addEventListener("click", removeMenuTab);
+  link.addEventListener('click', removeMenuTab);
 });
 
 desktopLinks.forEach((link) => {
-  link.addEventListener("click", select);
+  link.addEventListener('click', select);
 });
 
-let selectedLink = "";
+let selectedLink = '';
 
 function select() {
-  homeLink.classList.remove("selected");
+  homeLink.classList.remove('selected');
   if (selectedLink) {
-    selectedLink.classList.remove("selected");
+    selectedLink.classList.remove('selected');
     selectedLink = event.target;
-    selectedLink.classList.add("selected");
+    selectedLink.classList.add('selected');
   } else {
     selectedLink = event.target;
-    selectedLink.classList.add("selected");
+    selectedLink.classList.add('selected');
   }
 }
+
+hamburgerBtn.addEventListener('click', showMenuTab);
+removeBtn.addEventListener('click', removeMenuTab);
+homeLink.addEventListener('click', select);
