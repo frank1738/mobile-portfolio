2ee4b221f360870fac542f7f303eec2c59f42f2b
frank1738 <98453979+frank1738@users.noreply.github.com>
2022-04-04 15:52:31 +0300

    add new js file with new implementations

---
+++ index.js
@@ -0,0 +1,40 @@
+const mobileLinks=document.querySelectorAll('.mob-link')
+const menuTab=document.querySelector('.mob-menu')
+const hamburgerBtn=document.querySelector('.fa-bars')
+const removeBtn=document.querySelector('.fa-xmark')
+const desktopLinks=document.querySelectorAll('.menu-link')
+const homeLink=document.querySelector('.desktop')
+hamburgerBtn.addEventListener('click', showMenuTab)
+removeBtn.addEventListener('click', removeMenuTab)
+homeLink.classList.add('selected')
+homeLink.addEventListener('click', select)
+function showMenuTab(){
+    menuTab.style.width='100%'
+}
+
+function removeMenuTab(){
+    menuTab.style.width='0%'
+}
+
+mobileLinks.forEach((link)=>{
+    link.addEventListener('click', removeMenuTab)
+})
+
+desktopLinks.forEach((link)=>{
+    link.addEventListener('click', select)
+})
+
+let selectedLink=''
+
+function select(){
+    homeLink.classList.remove('selected')
+    if(selectedLink){
+        selectedLink.classList.remove('selected')
+        selectedLink=event.target
+        selectedLink.classList.add('selected')
+    }
+    else{
+        selectedLink=event.target
+        selectedLink.classList.add('selected')
+    }
+}
